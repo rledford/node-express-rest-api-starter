91fd5d6c6fd164f48dbc235d9535e2d2553225ab
rledford <ryan.c.ledford@gmail.com>
2018-08-31 11:14:15 -0500

    added /api response for mocha test

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/api', function(req, res) {
+  res.status(200).end('[8-)');
+});
+
 router.use('/api/v1', require('./users'));
 
 module.exports = router;
